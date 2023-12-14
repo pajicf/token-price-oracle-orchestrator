3d44f3c0dc15baf698b23a199bf40ea57c7e40ce
Filip Pajic <fica.pajic@gmail.com>
2023-12-14 16:07:04 +0100

    added ticker routes to api

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import { error } from "./middlewares/error.middleware";
 import StatusRoute from "./routes/status.route";
+import TickersRoute from "./routes/tickers.route";
+import TickersValidator from "./validators/tickers.validator";
 
 const v1 = Router();
 
+v1.get("/tickers/", TickersRoute.getTickerList);
+v1.get("/tickers/:tickerSymbol", TickersValidator.validateGetTicker, TickersRoute.getTicker);
+
 v1.get("/status", StatusRoute.getStatus);
+
+v1.use(error)
 
 export default v1;
---
+++ src/api/v1/validators/tickers.validator.ts
@@ -0,0 +1,19 @@
+import TickersRouteDefinitions from '../definitions/tickers.route';
+import {val} from "../middlewares/validate.middleware";
+import {checkSchema} from "express-validator";
+import {ValidatorFields} from "../../../types/util.types";
+
+type GetTickerFields =
+  (keyof TickersRouteDefinitions.TickerParams)
+
+const getTickerSchema: ValidatorFields<GetTickerFields> = {
+  tickerSymbol: {
+    in: ['params'],
+    errorMessage: 'Ticker must be a string',
+    isString: true
+  }
+}
+
+export default class TickersValidator {
+  public static validateGetTicker = val(checkSchema(getTickerSchema))
+}
